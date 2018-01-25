353e5b5f2ab07cbc7f4d72d6a19b730adb1d449c
meetkumarpatel.1504@gmail.com <meetkumarpatel.1504@gmail.com>
2018-01-25 14:37:32 -0800

    Final commit

---
+++ Section 16 - Building a SOM/som.py
@@ -0,0 +1,49 @@
+# Self Organizing Map
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Credit_Card_Applications.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Feature Scaling
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0, 1))
+X = sc.fit_transform(X)
+
+# Training the SOM
+from minisom import MiniSom
+som = MiniSom(x = 10, y = 10, input_len = 15, sigma = 1.0, learning_rate = 0.5)
+som.random_weights_init(X)
+som.train_random(data = X, num_iteration = 100)
+
+# Visualizing the results
+from pylab import bone, pcolor, colorbar, plot, show
+bone()
+pcolor(som.distance_map().T)
+colorbar()
+markers = ['o', 's']
+colors = ['r', 'g']
+for i, x in enumerate(X):
+    w = som.winner(x)
+    plot(w[0] + 0.5,
+         w[1] + 0.5,
+         markers[y[i]],
+         markeredgecolor = colors[y[i]],
+         markerfacecolor = 'None',
+         markersize = 10,
+         markeredgewidth = 2)
+show()
+
+# Finding the frauds
+mappings = som.win_map(X)
+#print(mappings)
+a = np.array(mappings[(8,1)])
+b = np.array(mappings[(6,8)])
+frauds = np.concatenate((a, b), axis = 0)
+frauds = sc.inverse_transform(frauds)
+np.savetxt('prediction.csv', frauds, fmt='%.2f', delimiter=',', header ="CustomerID, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14")
